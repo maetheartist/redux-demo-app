5ed6aa5b30cf725d6cd42b2f5b23b9c5c6835201
maetheartist <mariamkosemani1@gmail.com>
2025-02-27 12:15:21 +0100

    fix errors.

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import store from "../../../fip-seven/redux-app/src/redux/store";
+import {store}from "../src/redux/store"
 import { CounterProvider } from "./context/CounterContext";
 
 createRoot(document.getElementById("root")).render(
